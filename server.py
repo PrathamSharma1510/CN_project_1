6def48451c2521c0a453ed376246de27e4ae4097
g-an24 <pratham.sharma6113@gmail.com>
2023-02-12 02:03:23 -0500

    comments_add

--- server.py
+++ server.py
@@ -1,17 +1,24 @@
 import socket
 def start_server(port, BUFFER_SIZE):
-    print("Server is starting..")
+    # connecting the server
+    print("--------- Server is starting please wait ---------")
+    # socket connect
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(("localhost", port)) 
+    # listening the server 
     server.listen()
-    print("Server started. Listening on port", port)
+    print("Server started. Listening on port : ", port)
+    # connected server
+    # going into the loop
     while True:
         conn, add = server.accept()
-        print("[+] New client connected: ", add)
+        print("--------- New client connected at :", add, "---------")
         data =conn.recv(1024)
+        # data fetched
         command =data.decode("utf-8")
+        # splited into two parts command + filename
         data_split = command.split()
-        
+        # get function
         if data_split[0] == 'get':
             file = open(data_split[1], 'rb')
             file_chunks =file.read (1024)
@@ -20,6 +27,7 @@ def start_server(port, BUFFER_SIZE):
                 file_chunks = file.read(1024)
             file.close()
             print("File Sent!")
+        # upload function
         elif data_split[0] == 'upload':
             file = open('new' +data_split[1],'wb')
             file_chunks = conn.recv(1024)
@@ -28,6 +36,8 @@ def start_server(port, BUFFER_SIZE):
                 file_chunks = conn.recv(1024)
             file.close()
             print('File Received!')
+
+        # invalid function
         else :
             print('Invalid Command')
             conn.send("Invalid Command please try again!".encode("utf-8"))
